bdfb5c1edc75d3dfb84a927d3e4d5ec8cd454e2c
TerHent <timur10012@gmail.com>
2023-10-15 18:37:20 +0200

    print slot machine

--- main.py
+++ main.py
@@ -16,22 +16,33 @@ symbol_count = {
 
 def get_slot_machine_spin(rows, cols, symbols):
     all_symbols = []
-    for symbol, symbol_count in symbol.items():
+    for symbol, symbol_count in symbols.items():
         for _ in range(symbol_count):
             all_symbols.append(symbol)
 
     columns = []
-    current_cymbols = all_symbols[:]
     for _ in range(cols):
         column = []
+        current_symbols = all_symbols[:]
         for _ in range(rows):
-            value = random.choice(current_cymbols)
-            current_cymbols.remove(value)
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
             column.append(value)
 
         columns.append(column)            
 
     return columns
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], end=" | ")
+            else:
+                print(column[row], end="")
+
+        print()
+
 def deposit():
     while True:
         amount = input("What would you like to deposit? $")
@@ -60,16 +71,16 @@ def get_number_of_lines():
 
 def get_bet():
     while True:
-        bet = input("What would you like to bet on each line? $")
-        if bet.isdigit():
-            bet = int(bet)
-            if MIN_BET <= bet <= MAX_BET:
+        amount = input("What would you like to bet on each line? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
                 break
             else:
                 print(f"Bet must be between ${MIN_BET} - ${MAX_BET}")
         else:
             print("Please enter a number")
-    return bet
+    return amount
 
 def main():
     balance = deposit()
@@ -84,4 +95,7 @@ def main():
             break
     print(f"You are betting ${balance} on {lines} lines. Total bet is equal to: ${total_bet}")
 
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
+    
 main()
